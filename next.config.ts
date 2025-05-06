3757e7c20e93017908770db2500a08147dbf6e45
umairny <umairny1@gmail.com>
2025-05-06 15:45:10 -0400

    workflow added

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/pf25",
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
